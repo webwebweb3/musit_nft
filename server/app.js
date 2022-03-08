a5ab3ea22c6232bdbeaad06f56d944cb68caaf5f
Junhyuk Park <junhyeg08@gmail.com>
2022-03-09 00:04:03 +0900

    Feat: init server app

---
+++ server/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const { sequelize } = require('./models');
+
+dotenv.config();
+
+const Router = require('./routes');
+
+const app = express();
+
+app.set('port', process.env.PORT || 8002);
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  }),
+);
+
+app.use('/api', Router);
+
+/* 404 처리 */
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+/* error 처리 */
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.status(500).json({ err });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
