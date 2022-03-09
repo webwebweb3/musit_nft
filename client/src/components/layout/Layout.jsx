e3163226738d6cfcc9565f041aa7b31d37fbd09b
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-03-10 00:08:26 +0900

    Feat: add Login button

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,7 +1,35 @@
+import { Button } from "@mui/material";
 import { AppBar, Box, Divider, Toolbar } from "@mui/material";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+    const [account,setAccount] = useState("");
+
+    const getAccount = async () => {
+        try {
+            if (window.ethereum) {
+                const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                window.localStorage.setItem("address",accounts[0]);
+                 setAccount(accounts[0]);
+            } else {
+                alert("Install Metamask!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(()=>{
+        if(account === "") return;
+        const addr = window.localStorage.getItem("address");
+        if(addr !== null){
+            setAccount(addr);
+        }
+    },[account])
+
     return (
         <Box sx={{ display: "inline-block", width: "100%" }}>
             {/* header */}
@@ -12,9 +40,12 @@ const Layout = () => {
                 elevation={0}
             >
                 <Toolbar style={{ padding: 0, marginTop: "-5px" }}>
-                    <Box style={{ marginTop: "10px" }}></Box>
+                    <Box style={{ marginTop: "10px" }}>
+                    {account === "" ? <Button onClick={getAccount}>LOGIN</Button> : <Button>로그인완료</Button>}
+                    </Box>
                 </Toolbar>
                 <Divider sx={{ mt: 0.25, mb: 0.25, marginTop: "-7px" }} />
+                
             </AppBar>
 
             {/* main content */}
