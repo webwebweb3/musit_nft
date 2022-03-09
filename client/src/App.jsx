fc572808f4a9ff118ea0a3efbd824da72c7fd829
Junhyuk Park <junhyeg08@gmail.com>
2022-03-10 00:40:35 +0900

    Chore: 코드 정리

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Main from "./components/main/Main";
-import Layout from "./components/layout/Layout";
-import NotFound from "./components/notfound/NotFound";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Main from './components/main/Main';
+import Layout from './components/layout/Layout';
+import NotFound from './components/notfound/NotFound';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<Layout />}>
-                    <Route path="" element={<Main />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Layout />}>
+          <Route path="" element={<Main />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,64 +1,62 @@
-import { Button } from "@mui/material";
-import { AppBar, Box, Divider, Toolbar } from "@mui/material";
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Button } from '@mui/material';
+import { AppBar, Box, Divider, Toolbar } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    const [account,setAccount] = useState("");
+  const [account, setAccount] = useState('');
 
-    const getAccount = async () => {
-        try {
-            if (window.ethereum) {
-                const accounts = await window.ethereum.request({
-                    method: "eth_requestAccounts",
-                });
-                window.localStorage.setItem("address",accounts[0]);
-                 setAccount(accounts[0]);
-            } else {
-                alert("Install Metamask!");
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        window.localStorage.setItem('address', accounts[0]);
+        setAccount(accounts[0]);
+      } else {
+        alert('Install Metamask!');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(()=>{
-        if(account === "") return;
-        const addr = window.localStorage.getItem("address");
-        if(addr !== null){
-            setAccount(addr);
-        }
-    },[account])
+  useEffect(() => {
+    if (account === '') return;
+    const addr = window.localStorage.getItem('address');
+    if (addr !== null) {
+      setAccount(addr);
+    }
+  }, [account]);
 
-    return (
-        <Box sx={{ display: "inline-block", width: "100%" }}>
-            {/* header */}
-            <AppBar
-                enableColorOnDark
-                display="block"
-                color="inherit"
-                elevation={0}
-            >
-                <Toolbar style={{ padding: 0, marginTop: "-5px" }}>
-                    <Box style={{ marginTop: "10px" }}>
-                    {account === "" ? <Button onClick={getAccount}>LOGIN</Button> : <Button>로그인완료</Button>}
-                    </Box>
-                </Toolbar>
-                <Divider sx={{ mt: 0.25, mb: 0.25, marginTop: "-7px" }} />
-                
-            </AppBar>
+  return (
+    <Box sx={{ display: 'inline-block', width: '100%' }}>
+      {/* header */}
+      <AppBar enableColorOnDark display="block" color="inherit" elevation={0}>
+        <Toolbar style={{ padding: 0, marginTop: '-5px' }}>
+          <Box style={{ marginTop: '10px' }}>
+            {account === '' ? (
+              <Button onClick={getAccount}>LOGIN</Button>
+            ) : (
+              <Button>로그인완료</Button>
+            )}
+          </Box>
+        </Toolbar>
+        <Divider sx={{ mt: 0.25, mb: 0.25, marginTop: '-7px' }} />
+      </AppBar>
 
-            {/* main content */}
-            <Box
-                style={{
-                    marginLeft: "20px",
-                    marginTop: "100px",
-                }}
-            >
-                <Outlet />
-            </Box>
-        </Box>
-    );
+      {/* main content */}
+      <Box
+        style={{
+          marginLeft: '20px',
+          marginTop: '100px',
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
