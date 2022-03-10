de87b79251cc82727eb84cbfbb4dabe17402fd6f
KWANHOON LEE <back7930@gmail.com>
2022-03-10 15:36:31 +0900

    Chore: Register 모달창, box 없앰

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useEffect, useState } from 'react';
 import {
   AppBar,
@@ -7,13 +8,31 @@ import {
   Tabs,
   Tab,
   Button,
+  Modal,
+  Typography,
 } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import HeadsetIcon from '@mui/icons-material/Headset';
 import TokenIcon from '@mui/icons-material/Token';
+import TextField from '@mui/material/TextField';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Layout = () => {
   const [account, setAccount] = useState('');
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const getAccount = async () => {
     try {
@@ -50,45 +69,47 @@ const Layout = () => {
     >
       {/* header */}
       <AppBar
-        enableColorOnDark
-        display="block"
-        color="inherit"
-        elevation={0}
-        sx={{ background: '#06390' }}
         // enableColorOnDark
         // display="block"
         // color="inherit"
         // elevation={0}
+        sx={{ background: '#06390' }}
       >
-        <Toolbar style={{ padding: 0, marginTop: '-5px' }}>
-          <Box style={{ marginTop: '20px' }}>
-            {account === '' ? (
-              <>
-                <Tabs textColor="inherit">
-                  <Tab
-                    sx={{ marginLeft: '1400px' }}
-                    icon={<HeadsetIcon />}
-                    iconPosition="start"
-                    label="STREAMING"
-                  />
-                  <Tab icon={<TokenIcon />} iconPosition="start" label="NFT" />
-                </Tabs>
-                <Button
-                  sx={{ marginLeft: 'auto' }}
-                  onClick={getAccount}
-                  variant="contained"
-                >
-                  {' '}
-                  Login
-                </Button>
-                <Button variant="contained">Register</Button>
-              </>
-            ) : (
-              <Button>로그인완료</Button>
-            )}
-          </Box>
+        <Toolbar>
+          <img src="/images/logo.png" alt="logo" />
+          <Tabs textColor="inherit">
+            <Tab
+              icon={<HeadsetIcon />}
+              iconPosition="start"
+              label="STREAMING"
+            />
+            <Tab icon={<TokenIcon />} iconPosition="start" label="NFT" />
+          </Tabs>
+          <Button sx={{ marginLeft: 'auto' }} variant="contained">
+            Login
+          </Button>
+          <Button variant="contained" onClick={handleOpen}>
+            Register
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                장르
+              </Typography>
+              <TextField id="outlined-basic" label="장르" variant="outlined" />
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                국가
+              </Typography>
+              <TextField id="outlined-basic" label="국가" variant="outlined" />
+            </Box>
+          </Modal>
+          <Divider sx={{ mt: 0.25, mb: 0.25, marginTop: '-7px' }} />
         </Toolbar>
-        <Divider sx={{ mt: 0.25, mb: 0.25, marginTop: '-7px' }} />
       </AppBar>
 
       {/* main content */}
