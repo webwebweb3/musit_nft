c19fbf747cd51db07b150e91cb3428b5d61b3811
Junhyuk Park <junhyeg08@gmail.com>
2022-03-10 17:33:01 +0900

    Feat: 유저, 아티스트 DB 재수정

---
+++ server/models/user.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        metamask: {
+          type: Sequelize.STRING(42),
+          allowNull: true,
+          unique: true,
+        },
+        nationality: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        genre: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        img: {
+          type: Sequelize.STRING(200),
+          allowNull: true,
+        },
+        pass: {
+          type: Sequelize.STRING(200),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: true, // 삭제한척
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      },
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Artist);
+  }
+};
