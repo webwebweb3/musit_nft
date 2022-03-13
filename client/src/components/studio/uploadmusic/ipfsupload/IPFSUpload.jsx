78fcbce11075a550f3dba52263c14741f159ab8e
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-03-14 00:58:47 +0900

    Feat: add react-audio-player

--- client/src/components/studio/uploadmusic/ipfsupload/IPFSUpload.jsx
+++ client/src/components/studio/uploadmusic/ipfsupload/IPFSUpload.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { create } from 'ipfs-http-client';
 import { Button } from '@mui/material';
+import ReactAudioPlayer from 'react-audio-player';
+
 const client = create('https://ipfs.infura.io:5001/api/v0');
 
 const IPFSUpload = () => {
@@ -10,27 +12,30 @@ const IPFSUpload = () => {
   const handleMusicInput = e => {
     const file = e.target.files[0];
     const fileExt = file.name.split('.').pop();
+    console.log(file);
     if (
-      file.type === 'image/jpeg' ||
-      fileExt === 'jpg' ||
-      file.type === 'image/gif' ||
-      fileExt === 'gif' ||
-      file.type === 'image/png' ||
-      fileExt === 'png'
+      file.type === 'audio/mpeg' ||
+      fileExt === 'mp3' ||
+      file.type === 'audio/mp4' ||
+      fileExt === 'mp4' ||
+      file.type === 'audio/wav' ||
+      fileExt === 'wav' ||
+      file.type === 'audio/flac' ||
+      fileExt === 'flac'
     ) {
       setSelectedMusic(file);
       return;
     } else {
-      alert('이미지 파일만 업로드 가능합니다.');
+      alert('음악 파일만 업로드 가능합니다.');
       return;
     }
   };
 
-  const onChange = async e => {
-    const file = e.target.files[0];
+  const onChange1 = async () => {
+    console.log('1');
     try {
-      console.log(file);
-      const added = await client.add(file);
+      console.log(selectedMusic);
+      const added = await client.add(selectedMusic);
       const url = `https://ipfs.infura.io/ipfs/${added.path}`;
       updateFileUrl(url);
       console.log(fileUrl);
@@ -41,10 +46,9 @@ const IPFSUpload = () => {
 
   return (
     <div className="App">
-      <h1>IPFS Example</h1>
       <input type="file" onChange={handleMusicInput} />
-      <Button onClick={() => onChange}>IPFS 업로드</Button>
-      {fileUrl && <img src={fileUrl} width="600px" />}
+      <Button onClick={onChange1}>IPFS 업로드</Button>
+      {fileUrl && <ReactAudioPlayer src={fileUrl} controls />}
     </div>
   );
 };
