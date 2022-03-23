8540c8d55b996faa080f9381c2ea706dedf0437c
KWANHOON LEE <back7930@gmail.com>
2022-03-23 17:14:49 +0900

    FEAT: 재생목록

--- client/src/components/Pages/mypage/MyFavoriteTracks.jsx
+++ client/src/components/Pages/mypage/MyFavoriteTracks.jsx
@@ -8,10 +8,10 @@ import ListItemButton from '@mui/material/ListItemButton';
 //import CardMedia from '@mui/material/CardMedia';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+
 import Typography from '@mui/material/Typography';
 import { IconButton } from '@mui/material';
 
